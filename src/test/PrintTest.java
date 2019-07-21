1ea486d51b5969979296c5bb957bd6528f523caf
lnsylt <lnsylt@gmail.com>
2019-07-21 10:50:24 +0900

    Update PrintTest.java

--- src/test/PrintTest.java
+++ src/test/PrintTest.java
@@ -5,6 +5,7 @@ public class PrintTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is Print Test...");
+		System.out.println("ABCDDDDDD");
 	}
 
 }
